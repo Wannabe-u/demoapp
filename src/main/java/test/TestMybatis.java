6866da6e2d6ab25785408b53551db7278c262361
Wannabe-u <1103080854@qq.com>
2020-04-16 17:28:59 +0800

    5.0

--- src/main/java/test/TestMybatis.java
+++ src/main/java/test/TestMybatis.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,31 +20,25 @@ import java.io.InputStream;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationContext.xml")
-public class TestMybatis {
-    @Resource(name="sqlSessionFactory")
-    private  SqlSessionFactory sqlSessionFactory;
-
+public class TestMybatis extends BaseSessionFactoryImpl{
     @Test
     public void run1() throws IOException {
-        SqlSession ac = sqlSessionFactory.openSession();
+        SqlSession  ac = getSessionFactory();
         Account account = new Account();
-        account.setUser("Test2");
-        account.setPasswd("testtest2");
+        account.setUser("Test3");
+        account.setPasswd("testtest3");
         IAccountdao dao = ac.getMapper(IAccountdao.class);
         dao.saveAccount(account);
-        ac.close();
     }
 
     @Test
     public void run2() throws Exception {
-        SqlSession ac = sqlSessionFactory.openSession();
+        SqlSession  ac = getSessionFactory();
         IAccountdao dao = ac.getMapper(IAccountdao.class);
         List<Account> list = dao.findAll();
         for (Account account: list ) {
             System.out.println(account);
         }
-        ac.close();
     }
 
 }
