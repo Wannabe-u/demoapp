f5b5b2403d2eaf35ec3b7e80c74556393f748318
Wannabe-u <1103080854@qq.com>
2020-04-17 16:19:08 +0800

    5.0

--- src/main/java/test/TestMybatis.java
+++ src/main/java/test/TestMybatis.java
@@ -1,40 +1,44 @@
 package test;
 
-import com.mysql.cj.xdevapi.Session;
+
 import dao.IAccountdao;
-import dao.Impl.BaseSessionFactoryImpl;
 import domain.Account;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.exceptions.PersistenceException;
+
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
+@ContextConfiguration("classpath:applicationContext.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-public class TestMybatis extends BaseSessionFactoryImpl{
+public class TestMybatis {
+    @Resource(name="sqlSessionFactory")
+    private  SqlSessionFactory sqlSessionFactory;
+
+
     @Test
     public void run1() throws IOException {
-        SqlSession  ac = getSessionFactory();
         Account account = new Account();
-        account.setUser("Test3");
-        account.setPasswd("testtest3");
-        IAccountdao dao = ac.getMapper(IAccountdao.class);
-        dao.saveAccount(account);
+        try {
+            account.setUser("Test4");
+            account.setPasswd("testtest4");
+            IAccountdao dao = sqlSessionFactory.openSession().getMapper(IAccountdao.class);
+            dao.saveAccount(account);
+        }
+        catch (PersistenceException e){
+            System.out.println("用户名已存在");
+        }
     }
 
     @Test
     public void run2() throws Exception {
-        SqlSession  ac = getSessionFactory();
-        IAccountdao dao = ac.getMapper(IAccountdao.class);
+        IAccountdao dao = sqlSessionFactory.openSession().getMapper(IAccountdao.class);
         List<Account> list = dao.findAll();
         for (Account account: list ) {
             System.out.println(account);
